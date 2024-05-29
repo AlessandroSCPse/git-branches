2be75538824e0e182b2b82daf27eb0ef89acb630
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-05-29 12:15:48 +0200

    added proper description to homepage

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
     <h1>Benvenuti git branches nel mio progetto altro</h1>
 
     <h2>La nostra azienda fa progetti bellissimi con Laravel!</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque vitae cupiditate quasi, rem est, totam corporis quisquam eveniet praesentium alias aperiam dolorum ipsa illo vero, error at quidem ea!</p>
 </body>
 </html>
