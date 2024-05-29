ba2a860d7cf6834abef9fa1119a00300af9de6bc
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-05-29 12:29:53 +0200

    fixed lorem text

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 
     <h2>La nostra azienda fa progetti bellissimi con Laravel!</h2>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eaque vitae cupiditate quasi, rem est, totam corporis quisquam eveniet praesentium alias aperiam dolorum ipsa illo vero, error at quidem ea!</p>
+    <p>Dolor sit amet consectetur adipisicing elit. Deserunt eaque vitae cupiditate quasi, rem est, totam corporis quisquam eveniet praesentium alias aperiam dolorum ipsa illo vero, error at quidem ea!</p>
 </body>
 </html>
